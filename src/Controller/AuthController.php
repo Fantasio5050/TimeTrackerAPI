805fe4af41bd7a1faca5693ba68ee51f550fa49b
Ange Junia <angejunia23@gmail.com>
2025-03-25 14:57:31 +0100

    update

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -11,16 +11,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Validator\Constraints\Json;
 use Symfony\Component\HttpFoundation\Request;
 final class AuthController extends AbstractController
 {
 
-    #[Route('/api/register', name: 'app_auth_register', methods: ['POST', 'GET'])]
-    public function register(Request $request, UserRepository $userRepository, Security $security, EntityManagerInterface $entityManager): Response
+    #[Route('/api/register', name: 'app_auth_register', methods: ['POST'])]
+    public function register(Request $request, UserRepository $userRepository, Security $security, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
-        $userMail = $request->request->get('email');
+        $data = json_decode($request->getContent(), true);
+
+        if (!$data){
+            return new JsonResponse([
+                'error' => 'Invalid JSON',
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $userMail = $data['email'];
         if ($userRepository->findBy(['email' => $userMail])) {
             return new JsonResponse([
                 'error' => 'User already exists with this email',
@@ -28,27 +37,25 @@ final class AuthController extends AbstractController
         }
 
         try {
-            die(dump(json_encode($request->request->all())));
             $user = new User();
             $user->setEmail($userMail);
-            $user->setPassword($request->request->get('password'));
-            $user->setName($request->request->get('name'));
+            $user->setRoles(['ROLE_USER']);
+            $user->setPassword($passwordHasher->hashPassword($user, $data['password']));
+            $user->setName($data['name']);
             $user->setCreatedAt(new \DateTimeImmutable('now'));
             $user->setUpdatedAt(new \DateTimeImmutable('now'));
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // Generate JWT
-            $jwt = $this->get('lexik_jwt_authentication.jwt_manager')->create($user);
 
             // Return created user info and JWT
             return new JsonResponse([
                 'id' => $user->getId(),
                 'email' => $user->getEmail(),
+                'roles' => $user->getRoles(),
                 'name' => $user->getName(),
                 'created_at' => $user->getCreatedAt(),
                 'updated_at' => $user->getUpdatedAt(),
-                'jwt' => $jwt,
             ], Response::HTTP_CREATED);
 
         } catch (\Exception $e) {
@@ -58,16 +65,5 @@ final class AuthController extends AbstractController
         }
     }
 
-    #[Route('/api/login', name: 'app_auth_login')]
-    public function login(): JsonResponse
-    {
-        $user = $this->getUser();
-        $jwt = $this->get('lexik_jwt_authentication.jwt_manager')->create($user);
 
-        // Return the JWT
-        return new JsonResponse([
-            'jwt' => $jwt,
-        ]);
-
-    }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,18 +7,23 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ApiResource]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $email = null;
+
+    #[ORM\Column(type: 'json')]
+    private array $roles = [];
 
     #[ORM\Column(length: 255)]
     private ?string $name = null;
@@ -41,6 +46,58 @@ class User
     public function __construct()
     {
         $this->sessions = new ArrayCollection();
+    }
+
+    /**
+     * The public representation of the user (e.g. a username, an email address, etc.)
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
     public function getId(): ?int
@@ -131,16 +188,8 @@ class User
         return $this->jwt;
     }
 
-    public function getPassword(): ?string
+    public function checkPassword(string $password): bool
     {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): static
-    {
-        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
-        $this->password = $passwordHash;
-
-        return $this;
+        return password_verify($password, $this->password);
     }
 }
